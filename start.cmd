2ef806d2f6d7af77ef2992e400b062ef25dce6f3
Mustafa ÖNCEL <support@lifemcserver.com>
2019-11-10 19:28:28 +0300

    Release 2.0.2-BETA
    + Added an option for lowering RAM usage
    + Added an option for disabling server VM usage
    
    + Added performance settings section
      + Added an option for enabling 64-bit Java usage,
        fixes 32-bit system / VM support.
    
      + Added an option for disabling tiered compilation,
        since it is not available on some VMs.
    
      + Added an option for class caching, disabled by default.
    
      + Added an option for disabling the powershell usage.
    
    + Fixed a small bug with the online mode changing.
    
    Signed-off-by: Mustafa ÖNCEL <support@lifemcserver.com>

--- start.cmd
+++ start.cmd
@@ -7,7 +7,7 @@
 
 :: Sürüm - degistermeniz onerilmez
 
-set version=2.0.1-BETA
+set version=2.0.2-BETA
 
 :: Ayarlar - kendinize gore duzenleyebilirsiniz
 
@@ -15,7 +15,7 @@ set version=2.0.1-BETA
 set jar_name=craftbukkit
 
 :: Sunucunuzun surumu - 1.8.8, 1.9.4, 1.10.2, 1.11.2, 1.12.2, 1.13.2 veya 1.14.4 olabilir
-:: Not: Sadece yukarida belirtilen sunucu JAR dosyasi yok ise calisir.
+:: Not: Sadece yukarida belirtilen sunucu JAR dosyasi yok ise calisir
 set game_version=1.8.8
 
 :: Eger konsolun renkli olmasini istemiyor iseniz false yapabilirsiniz
@@ -29,6 +29,9 @@ set max_ram=1536M
 
 :: Kod onbellegi boyutu, 256M onerilir (MB icin M, GB icin G kullanin)
 set code_cache=256M
+
+:: Sunucunuzun daha az RAM yemesini fakat daha az performansli calismasini saglar
+set less_ram=false
 
 :: Sunucu kapandiktan sonra loglar temizlensin mi?
 set log_temizle=true
@@ -68,6 +71,23 @@ set verbose_info=false
 :: Eger sunucu dosyalariniz cok buyuk ise disk aktivitesini azaltmak icin kapatabilirsiniz
 set unblock_files=true
 
+:: Eger "This JVM instance does not support server VM." tarzi bir hata alirsaniz false yapin
+set use_server_vm=true
+
+:: Performans Ayarlari - duzenlemeniz ancak performans sorunu yasiyorsaniz onerilir
+
+:: Eger bilgisayariniz ve Java surumunuz 64-bit ise bunu true yapin
+set sixty_four_bit_java=false
+
+:: Sunucunuzun performansini arttiran bir ayar, fakat bazi Java surumlerinde calismaz
+set tiered_compilation=true
+
+:: Class dosyalarini onbellege alarak performans arttirir, fakat bazi sistemlerde calismaz
+set class_caching=false
+
+:: Baslatma kodu uzun suruyorsa veya powershell ile alakali hata veriyorsa true yapin
+set disable_powershell=false
+
 :: Mesajlar - kendinize gore duzenleyebilirsiniz
 
 :: Eger birden fazla sunucu penceresi aciyorsaniz karistirmamak icin bir onek girebilirsiniz
@@ -87,7 +107,7 @@ set dosya_indiriliyor=Sunucu dosyasi indiriliyor...
 set temizlik_yapiliyor=Temizlik yapiliyor...
 set yeniden_baslatiliyor=Sunucu yeniden baslatiliyor...
 
-:: Kod kismi - duzenlemeniz onerilmez.
+:: Kod kismi - duzenlemeniz onerilmez
 
 call chcp 65001 > nul
 call title %title%
@@ -102,25 +122,26 @@ if %konsol_renkleri% equ false call set additional_parameters= -nojline
 
 if not exist %jar_name%.jar (
  call echo %dosya_indiriliyor%
- call powershell -nologo -noninteractive -executionpolicy bypass -command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::TLS12; (New-Object Net.WebClient).DownloadFile('%download_url%', '%jar_name%.jar')" 1> nul
+ if %disable_powershell% equ false call powershell -nologo -noninteractive -executionpolicy bypass -command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::TLS12; (New-Object Net.WebClient).DownloadFile('%download_url%', '%jar_name%.jar')" 1> nul
 )
 
 call echo eula=true> eula.txt
 
 if exist %spigot_config% call find "verbose: true" %spigot_config% 1> nul
-if exist %spigot_config% if %errorlevel% equ 1 if %verbose% equ false call powershell -nologo -noninteractive -executionpolicy bypass -command "(gc spigot.yml) -replace 'verbose: true', 'verbose: false' | Out-File -encoding UTF8 spigot.yml" 1> nul
+if exist %spigot_config% if %errorlevel% equ 1 if %verbose% equ false if %disable_powershell% equ false call powershell -nologo -noninteractive -executionpolicy bypass -command "(gc spigot.yml) -replace 'verbose: true', 'verbose: false' | Out-File -encoding UTF8 spigot.yml" 1> nul
 
 if exist %paper_config% call find "verbose: true" %paper_config% 1> nul
-if exist %paper_config% if %errorlevel% equ 1 if %verbose% equ false call powershell -nologo -noninteractive -executionpolicy bypass -command "(gc paper.yml) -replace 'verbose: true', 'verbose: false' | Out-File -encoding UTF8 paper.yml" 1> nul
+if exist %paper_config% if %errorlevel% equ 1 if %verbose% equ false if %disable_powershell% equ false call powershell -nologo -noninteractive -executionpolicy bypass -command "(gc paper.yml) -replace 'verbose: true', 'verbose: false' | Out-File -encoding UTF8 paper.yml" 1> nul
 
 call set server_config=server.properties
 
 if exist %server_config% if %online_mode% equ false call find "online-mode=true" %server_config% 1> nul
-if exist %server_config% if %errorlevel% equ 1 if %verbose% equ false call powershell -nologo -noninteractive -executionpolicy bypass -command "(gc server.properties) -replace 'online-mode=true', 'online-mode=false' | Out-File -encoding UTF8 server.properties" 1> nul
+if exist %server_config% if %errorlevel% equ 1 if %online_mode% equ false if %disable_powershell% equ false call powershell -nologo -noninteractive -executionpolicy bypass -command "(gc server.properties) -replace 'online-mode=true', 'online-mode=false' | Out-File -encoding UTF8 server.properties" 1> nul
 
 if %unblock_files% equ true if %unblocked% equ false (
  if %verbose_info% equ true call echo Ublocking files in the background...
- call powershell -nologo -noninteractive -executionpolicy bypass -command "Start-Job -Name 'Unblock Files' -ScriptBlock { [System.Threading.Thread]::CurrentThread.Priority = 'BelowNormal'; ([System.Diagnostics.Process]::GetCurrentProcess()).PriorityClass = 'BelowNormal'; Get-ChildItem -Recurse | Unblock-File }" 1> nul
+ if %disable_powershell% equ false call powershell -nologo -noninteractive -executionpolicy bypass -command "Start-Job -Name 'Unblock Files' -ScriptBlock { [System.Threading.Thread]::CurrentThread.Priority = 'BelowNormal'; ([System.Diagnostics.Process]::GetCurrentProcess()).PriorityClass = 'BelowNormal'; Get-ChildItem -Recurse | Unblock-File }" 1> nul
+
  call set unblocked=true
 )
 
@@ -128,7 +149,28 @@ call set module_access=
 if %allow_module_access% equ true call set module_access= --add-opens java.base/java.lang=ALL-UNNAMED
 
 if %verbose_info% equ true call echo Starting server with minimum RAM of %min_ram% and maximum of %max_ram%, code cache is %code_cache%
-call java -XX:+UnlockExperimentalVMOptions -XX:+IgnoreUnrecognizedVMOptions -XX:+IdleTuningGcOnIdle%module_access% -Xshare:on -Xshareclasses -d64 -server -Xms%min_ram% -Xmx%max_ram% -XX:ReservedCodeCacheSize=%code_cache% -XX:UseSSE=4 -XX:+UseGCOverheadLimit -XX:+MaxFDLimit -XX:+RelaxAccessControlCheck -XX:+UseThreadPriorities -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=MojangTricksIntelDriversForPerformance_javaw.exe_minecraft.exe -XX:+TieredCompilation -XX:+UseLWPSynchronization -XX:+UseBiasedLocking -XX:+UseFastAccessorMethods -XX:+CMSIncrementalPacing -XX:+CMSParallelRemarkEnabled -XX:+UseCMSInitiatingOccupancyOnly -XX:+ScavengeBeforeFullGC -XX:+CMSScavengeBeforeRemark -XX:+CMSClassUnloadingEnabled -XX:+UseStringDeduplication -XX:+ParallelRefProcEnabled -XX:+OmitStackTraceInFastThrow -XX:-AggressiveOpts -XX:+DisableExplicitGC -XX:+UseGCStartupHints -XX:+ShareAnonymousClasses -Dcom.ibm.enableClassCaching=true -XX+JITInlineWatches -XX+TransparentHugePage -Dsun.io.useCanonPrefixCache=false -Djava.net.preferIPv4Stack=true -Dsun.net.http.errorstream.enableBuffering=true -Dsun.net.client.defaultConnectTimeout=5000 -Dsun.net.client.defaultReadTimeout=5000 -Dskript.dontUseNamesForSerialization=true -Djava.net.preferIPv4Stack=true -Djdk.http.auth.tunneling.disabledSchemes="" -Djdk.attach.allowAttachSelf -Dkotlinx.coroutines.debug=off -Djava.awt.headless=%head_less% -Dfile.encoding=UTF-8 -Dsun.jnu.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Duser.language=en -Duser.country=US -Duser.timezone=Asia/Istanbul -Dpaper.playerconnection.keepalive=%io_timeout% -Dnashorn.option.no.deprecation.warning=true -Dnashorn.args=--no-deprecation-warning -Dlog4j.skipJansi=true -Dusing.flags.lifemcserver.com=true -Djansi.force=true -Dansi.force=true -Dlibrary.jansi.version=%jar_name% -jar %jar_name%.jar "-o %online_mode% --log-append=false --log-strip-color nogui%additional_parameters%"
+
+if %sixty_four_bit_java% equ true call set sixty_four_bit_java0= -d64
+if %tiered_compilation% equ true call set tiered_compilation0= -XX:+TieredCompilation
+
+if %class_caching% equ true (
+ call set class_caching0= -Xshare:on -Xshareclasses
+ call set class_caching1= -XX:+ShareAnonymousClasses -Dcom.ibm.enableClassCaching=true
+)
+
+if %less_ram% equ false (
+ call set less_ram0= -XX:+UseStringDeduplication
+ call set less_ram1= -XX:+DisableExplicitGC
+) else (
+ call set min_ram=1M
+)
+
+if %use_server_vm% equ true call set use_server_vm0= -server
+
+call set full_arguments=-XX:+UnlockExperimentalVMOptions -XX:+IgnoreUnrecognizedVMOptions -XX:+IdleTuningGcOnIdle%module_access%%class_caching0%%sixty_four_bit_java0%%use_server_vm0% -Xms%min_ram% -Xmx%max_ram% -XX:ReservedCodeCacheSize=%code_cache% -XX:UseSSE=4 -XX:+UseGCOverheadLimit -XX:+MaxFDLimit -XX:+RelaxAccessControlCheck -XX:+UseThreadPriorities -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=MojangTricksIntelDriversForPerformance_javaw.exe_minecraft.exe%tiered_compilation0% -XX:+UseLWPSynchronization -XX:+UseBiasedLocking -XX:+UseFastAccessorMethods -XX:+CMSIncrementalPacing -XX:+CMSParallelRemarkEnabled -XX:+UseCMSInitiatingOccupancyOnly -XX:+ScavengeBeforeFullGC -XX:+CMSScavengeBeforeRemark -XX:+CMSClassUnloadingEnabled%less_ram0% -XX:+ParallelRefProcEnabled -XX:+OmitStackTraceInFastThrow -XX:-AggressiveOpts%less_ram1% -XX:+UseGCStartupHints%class_caching1% -XX+JITInlineWatches -XX+TransparentHugePage -Dsun.io.useCanonPrefixCache=false -Djava.net.preferIPv4Stack=true -Dsun.net.http.errorstream.enableBuffering=true -Dsun.net.client.defaultConnectTimeout=5000 -Dsun.net.client.defaultReadTimeout=5000 -Dskript.dontUseNamesForSerialization=true -Djava.net.preferIPv4Stack=true -Djdk.http.auth.tunneling.disabledSchemes="" -Djdk.attach.allowAttachSelf -Dkotlinx.coroutines.debug=off -Djava.awt.headless=%head_less% -Dfile.encoding=UTF-8 -Dsun.jnu.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Duser.language=en -Duser.country=US -Duser.timezone=Asia/Istanbul -Dpaper.playerconnection.keepalive=%io_timeout% -Dnashorn.option.no.deprecation.warning=true -Dnashorn.args=--no-deprecation-warning -Dlog4j.skipJansi=true -Dusing.flags.lifemcserver.com=true -Djansi.force=true -Dansi.force=true -Dlibrary.jansi.version=%jar_name% -jar %jar_name%.jar "-o %online_mode% --log-append=false --log-strip-color nogui%additional_parameters%"
+if %verbose_info% equ true call echo Starting Java with the final arguments of %full_arguments%
+
+call java %full_arguments%
 
 call echo %temizlik_yapiliyor%
 
