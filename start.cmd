a66f83b6d9163d56dc9b2c93092bdf10a49714ef
Mustafa ÖNCEL <support@lifemcserver.com>
2019-11-27 18:12:21 +0300

    Release 2.0.3-BETA
    + Added an option for enabling assertions
    + Suppressed command outputs
    + Fixed a typo in a verbose message
    + Now shows Java version on startup
    + Fixed DeluxeMenus and other JS/Nashorn compatibility
    
    Signed-off-by: Mustafa ÖNCEL <support@lifemcserver.com>

--- start.cmd
+++ start.cmd
@@ -1,4 +1,5 @@
 @echo off
+call chcp 65001 > nul
 
 :: Bu kismi ellemeniz onerilmez
 
@@ -7,7 +8,7 @@
 
 :: Sürüm - degistermeniz onerilmez
 
-set version=2.0.2-BETA
+set version=2.0.3-BETA
 
 :: Ayarlar - kendinize gore duzenleyebilirsiniz
 
@@ -74,6 +75,9 @@ set unblock_files=true
 :: Eger "This JVM instance does not support server VM." tarzi bir hata alirsaniz false yapin
 set use_server_vm=true
 
+:: Assertion ozelligini acar. Sadece gelistirici iseniz acin, sunucudaki hatalari arttirabilir
+set enable_assertions=false
+
 :: Performans Ayarlari - duzenlemeniz ancak performans sorunu yasiyorsaniz onerilir
 
 :: Eger bilgisayariniz ve Java surumunuz 64-bit ise bunu true yapin
@@ -109,13 +113,12 @@ set yeniden_baslatiliyor=Sunucu yeniden baslatiliyor...
 
 :: Kod kismi - duzenlemeniz onerilmez
 
-call chcp 65001 > nul
 call title %title%
-
 call set unblocked=false
 
 :start
 call echo %baslatiliyor%
+call echo.
 
 call set additional_parameters=
 if %konsol_renkleri% equ false call set additional_parameters= -nojline
@@ -127,19 +130,19 @@ if not exist %jar_name%.jar (
 
 call echo eula=true> eula.txt
 
-if exist %spigot_config% call find "verbose: true" %spigot_config% 1> nul
+if exist %spigot_config% call find "verbose: true" %spigot_config% 1> nul 2> nul
 if exist %spigot_config% if %errorlevel% equ 1 if %verbose% equ false if %disable_powershell% equ false call powershell -nologo -noninteractive -executionpolicy bypass -command "(gc spigot.yml) -replace 'verbose: true', 'verbose: false' | Out-File -encoding UTF8 spigot.yml" 1> nul
 
-if exist %paper_config% call find "verbose: true" %paper_config% 1> nul
+if exist %paper_config% call find "verbose: true" %paper_config% 1> nul 2> nul
 if exist %paper_config% if %errorlevel% equ 1 if %verbose% equ false if %disable_powershell% equ false call powershell -nologo -noninteractive -executionpolicy bypass -command "(gc paper.yml) -replace 'verbose: true', 'verbose: false' | Out-File -encoding UTF8 paper.yml" 1> nul
 
 call set server_config=server.properties
 
-if exist %server_config% if %online_mode% equ false call find "online-mode=true" %server_config% 1> nul
+if exist %server_config% if %online_mode% equ false call find "online-mode=true" %server_config% 1> nul 2> nul
 if exist %server_config% if %errorlevel% equ 1 if %online_mode% equ false if %disable_powershell% equ false call powershell -nologo -noninteractive -executionpolicy bypass -command "(gc server.properties) -replace 'online-mode=true', 'online-mode=false' | Out-File -encoding UTF8 server.properties" 1> nul
 
 if %unblock_files% equ true if %unblocked% equ false (
- if %verbose_info% equ true call echo Ublocking files in the background...
+ if %verbose_info% equ true call echo Unblocking files in the background...
  if %disable_powershell% equ false call powershell -nologo -noninteractive -executionpolicy bypass -command "Start-Job -Name 'Unblock Files' -ScriptBlock { [System.Threading.Thread]::CurrentThread.Priority = 'BelowNormal'; ([System.Diagnostics.Process]::GetCurrentProcess()).PriorityClass = 'BelowNormal'; Get-ChildItem -Recurse | Unblock-File }" 1> nul
 
  call set unblocked=true
@@ -166,21 +169,22 @@ if %less_ram% equ false (
 )
 
 if %use_server_vm% equ true call set use_server_vm0= -server
+if %enable_assertions% equ true call set enable_assertions0= -esa -ea -Xfuture -Xverify:all
 
-call set full_arguments=-XX:+UnlockExperimentalVMOptions -XX:+IgnoreUnrecognizedVMOptions -XX:+IdleTuningGcOnIdle%module_access%%class_caching0%%sixty_four_bit_java0%%use_server_vm0% -Xms%min_ram% -Xmx%max_ram% -XX:ReservedCodeCacheSize=%code_cache% -XX:UseSSE=4 -XX:+UseGCOverheadLimit -XX:+MaxFDLimit -XX:+RelaxAccessControlCheck -XX:+UseThreadPriorities -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=MojangTricksIntelDriversForPerformance_javaw.exe_minecraft.exe%tiered_compilation0% -XX:+UseLWPSynchronization -XX:+UseBiasedLocking -XX:+UseFastAccessorMethods -XX:+CMSIncrementalPacing -XX:+CMSParallelRemarkEnabled -XX:+UseCMSInitiatingOccupancyOnly -XX:+ScavengeBeforeFullGC -XX:+CMSScavengeBeforeRemark -XX:+CMSClassUnloadingEnabled%less_ram0% -XX:+ParallelRefProcEnabled -XX:+OmitStackTraceInFastThrow -XX:-AggressiveOpts%less_ram1% -XX:+UseGCStartupHints%class_caching1% -XX+JITInlineWatches -XX+TransparentHugePage -Dsun.io.useCanonPrefixCache=false -Djava.net.preferIPv4Stack=true -Dsun.net.http.errorstream.enableBuffering=true -Dsun.net.client.defaultConnectTimeout=5000 -Dsun.net.client.defaultReadTimeout=5000 -Dskript.dontUseNamesForSerialization=true -Djava.net.preferIPv4Stack=true -Djdk.http.auth.tunneling.disabledSchemes="" -Djdk.attach.allowAttachSelf -Dkotlinx.coroutines.debug=off -Djava.awt.headless=%head_less% -Dfile.encoding=UTF-8 -Dsun.jnu.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Duser.language=en -Duser.country=US -Duser.timezone=Asia/Istanbul -Dpaper.playerconnection.keepalive=%io_timeout% -Dnashorn.option.no.deprecation.warning=true -Dnashorn.args=--no-deprecation-warning -Dlog4j.skipJansi=true -Dusing.flags.lifemcserver.com=true -Djansi.force=true -Dansi.force=true -Dlibrary.jansi.version=%jar_name% -jar %jar_name%.jar "-o %online_mode% --log-append=false --log-strip-color nogui%additional_parameters%"
+call set full_arguments=-showversion -XX:+UnlockExperimentalVMOptions -XX:+IgnoreUnrecognizedVMOptions%enable_assertions0% -XX:+IdleTuningGcOnIdle -XX:+ShowMessageBoxOnError -Xlint:none%module_access%%class_caching0%%sixty_four_bit_java0%%use_server_vm0% -Xms%min_ram% -Xmx%max_ram% -XX:ReservedCodeCacheSize=%code_cache% -XX:UseSSE=4 -XX:+UseGCOverheadLimit -XX:+MaxFDLimit -XX:+RelaxAccessControlCheck -XX:+UseThreadPriorities -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=MojangTricksIntelDriversForPerformance_javaw.exe_minecraft.exe%tiered_compilation0% -XX:+UseLWPSynchronization -XX:+UseBiasedLocking -XX:+UseFastAccessorMethods -XX:+CMSIncrementalPacing -XX:+CMSParallelRemarkEnabled -XX:+UseCMSInitiatingOccupancyOnly -XX:+ScavengeBeforeFullGC -XX:+CMSScavengeBeforeRemark -XX:+CMSClassUnloadingEnabled%less_ram0% -XX:+ParallelRefProcEnabled -XX:+OmitStackTraceInFastThrow -XX:-AggressiveOpts%less_ram1% -XX:+UseGCStartupHints%class_caching1% -XX+JITInlineWatches -XX+TransparentHugePage -Dsun.io.useCanonPrefixCache=false -Djava.net.preferIPv4Stack=true -Dsun.net.http.errorstream.enableBuffering=true -Dsun.net.client.defaultConnectTimeout=5000 -Dsun.net.client.defaultReadTimeout=5000 -Dskript.dontUseNamesForSerialization=true -Djava.net.preferIPv4Stack=true -Djdk.http.auth.tunneling.disabledSchemes="" -Djdk.attach.allowAttachSelf -Dkotlinx.coroutines.debug=off -Djava.awt.headless=%head_less% -Dfile.encoding=UTF-8 -Dsun.jnu.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Duser.language=en -Duser.country=US -Duser.timezone=Asia/Istanbul -Dpaper.playerconnection.keepalive=%io_timeout% -Dnashorn.option.no.deprecation.warning=true -Dlog4j.skipJansi=true -Dusing.flags.lifemcserver.com=true -Djansi.force=true -Dansi.force=true -Dlibrary.jansi.version=%jar_name% -jar %jar_name%.jar "-o %online_mode% --log-append=false --log-strip-color nogui%additional_parameters%"
 if %verbose_info% equ true call echo Starting Java with the final arguments of %full_arguments%
 
 call java %full_arguments%
 
 call echo %temizlik_yapiliyor%
 
-if %log_temizle% equ true call del logs\*.* /q 2> nul
-call del plugins\NoCheatPlus\*.log /q 2> nul
-call del plugins\NoCheatPlus\*.lck /q 2> nul
-call del plugins\NoCheatPlus\*.log.* /q 2> nul
-call del plugins\AuthMe\authme.log /q 2> nul
-call del plugins\bStats\temp.txt /q 2> nul
-call del plugins\AntiAura\logs\*.* /q 2> nul
+if %log_temizle% equ true call del logs\*.* /q 1> nul 2> nul
+call del plugins\NoCheatPlus\*.log /q 1> nul 2> nul
+call del plugins\NoCheatPlus\*.lck /q 1> nul 2> nul
+call del plugins\NoCheatPlus\*.log.* /q 1> nul 2> nul
+call del plugins\AuthMe\authme.log /q 1> nul 2> nul
+call del plugins\bStats\temp.txt /q 1> nul 2> nul
+call del plugins\AntiAura\logs\*.* /q 1> nul 2> nul
 
 call echo %yeniden_baslatiliyor%
 call timeout %delay% 2> nul
