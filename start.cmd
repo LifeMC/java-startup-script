e264a4da749a2ad6cc6783393b5ae27af93b44d2
Mustafa ÖNCEL <support@lifemcserver.com>
2019-12-11 10:46:33 +0300

    Release 2.0.4-BETA
    + Fixed a rare encoding error
    + Maked all option names English
    
    + Moved colored console options location
    + Added an option for disabling auto deletion of files
    
    + Added an option for disabling auto restart
    + Added an option for showing message box on error
    
    + Added an option for disabling printion of Java version at startup
    
    + Fixed class sharing errors
    + Added an option for -XX:(+/-)OmitStackTraceInFastThrow
    
    Signed-off-by: Mustafa ÖNCEL <support@lifemcserver.com>

--- start.cmd
+++ start.cmd
@@ -1,5 +1,5 @@
 @echo off
-call chcp 65001 > nul
+@call @chcp 65001 > nul
 
 :: Bu kismi ellemeniz onerilmez
 
@@ -8,7 +8,7 @@ call chcp 65001 > nul
 
 :: Sürüm - degistermeniz onerilmez
 
-set version=2.0.3-BETA
+set version=2.0.4-BETA
 
 :: Ayarlar - kendinize gore duzenleyebilirsiniz
 
@@ -18,9 +18,6 @@ set jar_name=craftbukkit
 :: Sunucunuzun surumu - 1.8.8, 1.9.4, 1.10.2, 1.11.2, 1.12.2, 1.13.2 veya 1.14.4 olabilir
 :: Not: Sadece yukarida belirtilen sunucu JAR dosyasi yok ise calisir
 set game_version=1.8.8
-
-:: Eger konsolun renkli olmasini istemiyor iseniz false yapabilirsiniz
-set konsol_renkleri=true
 
 :: Sunucunun kullanacagi minimum ram miktari (MB icin M, GB icin G kullanin)
 set min_ram=1536M
@@ -34,8 +31,17 @@ set code_cache=256M
 :: Sunucunuzun daha az RAM yemesini fakat daha az performansli calismasini saglar
 set less_ram=false
 
+:: Eger konsolun renkli olmasini istemiyor iseniz false yapabilirsiniz
+set colored_console=true
+
 :: Sunucu kapandiktan sonra loglar temizlensin mi?
-set log_temizle=true
+set clear_logs=true
+
+:: Sunucu kapandiktan sonra gereksiz dosyalar silinsin mi?
+set auto_del_files=true
+
+:: Sunucu kapandiginda otomatik tekrar acar, cokme vb. durumlarda ise yarayabilir
+set auto_restart=true
 
 :: Sunucu kapandiktan ne kadar sonra yeniden baslatilsin? (saniye)
 set delay=5
@@ -78,6 +84,12 @@ set use_server_vm=true
 :: Assertion ozelligini acar. Sadece gelistirici iseniz acin, sunucudaki hatalari arttirabilir
 set enable_assertions=false
 
+:: Hata ciktiginda mesaj kutusu gosterir, sunucuyu debug islemleri icin acik tutar
+set messagebox_on_error=false
+
+:: Eger baslangicta Java surumunun yazdirilmasini istemiyorsaniz false yapin
+set print_java_version=true
+
 :: Performans Ayarlari - duzenlemeniz ancak performans sorunu yasiyorsaniz onerilir
 
 :: Eger bilgisayariniz ve Java surumunuz 64-bit ise bunu true yapin
@@ -85,6 +97,9 @@ set sixty_four_bit_java=false
 
 :: Sunucunuzun performansini arttiran bir ayar, fakat bazi Java surumlerinde calismaz
 set tiered_compilation=true
+
+:: Surekli tekrarlanan hatalarda, hatanin bir kismini gizler. Gelistiriciler soyler ise kapatin
+set omit_stacktrace=true
 
 :: Class dosyalarini onbellege alarak performans arttirir, fakat bazi sistemlerde calismaz
 set class_caching=false
@@ -121,7 +136,7 @@ call echo %baslatiliyor%
 call echo.
 
 call set additional_parameters=
-if %konsol_renkleri% equ false call set additional_parameters= -nojline
+if %colored_console% equ false call set additional_parameters= -nojline
 
 if not exist %jar_name%.jar (
  call echo %dosya_indiriliyor%
@@ -158,7 +173,7 @@ if %tiered_compilation% equ true call set tiered_compilation0= -XX:+TieredCompil
 
 if %class_caching% equ true (
  call set class_caching0= -Xshare:on -Xshareclasses
- call set class_caching1= -XX:+ShareAnonymousClasses -Dcom.ibm.enableClassCaching=true
+ call set class_caching1= -XX:+ShareAnonymousClasses -XX+TransparentHugePage -Dcom.ibm.enableClassCaching=true
 )
 
 if %less_ram% equ false (
@@ -171,22 +186,33 @@ if %less_ram% equ false (
 if %use_server_vm% equ true call set use_server_vm0= -server
 if %enable_assertions% equ true call set enable_assertions0= -esa -ea -Xfuture -Xverify:all
 
-call set full_arguments=-showversion -XX:+UnlockExperimentalVMOptions -XX:+IgnoreUnrecognizedVMOptions%enable_assertions0% -XX:+IdleTuningGcOnIdle -XX:+ShowMessageBoxOnError -Xlint:none%module_access%%class_caching0%%sixty_four_bit_java0%%use_server_vm0% -Xms%min_ram% -Xmx%max_ram% -XX:ReservedCodeCacheSize=%code_cache% -XX:UseSSE=4 -XX:+UseGCOverheadLimit -XX:+MaxFDLimit -XX:+RelaxAccessControlCheck -XX:+UseThreadPriorities -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=MojangTricksIntelDriversForPerformance_javaw.exe_minecraft.exe%tiered_compilation0% -XX:+UseLWPSynchronization -XX:+UseBiasedLocking -XX:+UseFastAccessorMethods -XX:+CMSIncrementalPacing -XX:+CMSParallelRemarkEnabled -XX:+UseCMSInitiatingOccupancyOnly -XX:+ScavengeBeforeFullGC -XX:+CMSScavengeBeforeRemark -XX:+CMSClassUnloadingEnabled%less_ram0% -XX:+ParallelRefProcEnabled -XX:+OmitStackTraceInFastThrow -XX:-AggressiveOpts%less_ram1% -XX:+UseGCStartupHints%class_caching1% -XX+JITInlineWatches -XX+TransparentHugePage -Dsun.io.useCanonPrefixCache=false -Djava.net.preferIPv4Stack=true -Dsun.net.http.errorstream.enableBuffering=true -Dsun.net.client.defaultConnectTimeout=5000 -Dsun.net.client.defaultReadTimeout=5000 -Dskript.dontUseNamesForSerialization=true -Djava.net.preferIPv4Stack=true -Djdk.http.auth.tunneling.disabledSchemes="" -Djdk.attach.allowAttachSelf -Dkotlinx.coroutines.debug=off -Djava.awt.headless=%head_less% -Dfile.encoding=UTF-8 -Dsun.jnu.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Duser.language=en -Duser.country=US -Duser.timezone=Asia/Istanbul -Dpaper.playerconnection.keepalive=%io_timeout% -Dnashorn.option.no.deprecation.warning=true -Dlog4j.skipJansi=true -Dusing.flags.lifemcserver.com=true -Djansi.force=true -Dansi.force=true -Dlibrary.jansi.version=%jar_name% -jar %jar_name%.jar "-o %online_mode% --log-append=false --log-strip-color nogui%additional_parameters%"
+if %omit_stacktrace% equ true call set omit_stacktrace0= -XX:+OmitStackTraceInFastThrow
+if %messagebox_on_error% equ true call set show_messagebox_onerror0= -XX:+ShowMessageBoxOnError
+
+call set full_arguments=-XX:+UnlockExperimentalVMOptions -XX:+IgnoreUnrecognizedVMOptions%enable_assertions0% -XX:+IdleTuningGcOnIdle%show_messagebox_onerror0% -Xlint:none%module_access%%class_caching0%%sixty_four_bit_java0%%use_server_vm0% -Xms%min_ram% -Xmx%max_ram% -XX:ReservedCodeCacheSize=%code_cache% -XX:UseSSE=4 -XX:+UseGCOverheadLimit -XX:+MaxFDLimit -XX:+RelaxAccessControlCheck -XX:+UseThreadPriorities -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=MojangTricksIntelDriversForPerformance_javaw.exe_minecraft.exe%tiered_compilation0% -XX:+UseLWPSynchronization -XX:+UseBiasedLocking -XX:+UseFastAccessorMethods -XX:+CMSIncrementalPacing -XX:+CMSParallelRemarkEnabled -XX:+UseCMSInitiatingOccupancyOnly -XX:+ScavengeBeforeFullGC -XX:+CMSScavengeBeforeRemark -XX:+CMSClassUnloadingEnabled%less_ram0% -XX:+ParallelRefProcEnabled%omit_stacktrace0% -XX:-AggressiveOpts%less_ram1% -XX:+UseGCStartupHints%class_caching1% -XX+JITInlineWatches -Dsun.io.useCanonPrefixCache=false -Djava.net.preferIPv4Stack=true -Dsun.net.http.errorstream.enableBuffering=true -Dsun.net.client.defaultConnectTimeout=5000 -Dsun.net.client.defaultReadTimeout=5000 -Dskript.dontUseNamesForSerialization=true -Djava.net.preferIPv4Stack=true -Djdk.http.auth.tunneling.disabledSchemes="" -Djdk.attach.allowAttachSelf -Dkotlinx.coroutines.debug=off -Djava.awt.headless=%head_less% -Dfile.encoding=UTF-8 -Dsun.jnu.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Duser.language=en -Duser.country=US -Duser.timezone=Asia/Istanbul -Dpaper.playerconnection.keepalive=%io_timeout% -Dnashorn.option.no.deprecation.warning=true -Dlog4j.skipJansi=true -Dusing.flags.lifemcserver.com=true -Dflags.lifemcserver.com.version="%version%" -Djansi.force=true -Dansi.force=true -Dlibrary.jansi.version=%jar_name% -jar %jar_name%.jar "-o %online_mode% --log-append=false --log-strip-color nogui%additional_parameters%"
+if %print_java_version% equ true call set full_arguments=-showversion %full_arguments%
+
 if %verbose_info% equ true call echo Starting Java with the final arguments of %full_arguments%
 
 call java %full_arguments%
 
 call echo %temizlik_yapiliyor%
 
-if %log_temizle% equ true call del logs\*.* /q 1> nul 2> nul
-call del plugins\NoCheatPlus\*.log /q 1> nul 2> nul
-call del plugins\NoCheatPlus\*.lck /q 1> nul 2> nul
-call del plugins\NoCheatPlus\*.log.* /q 1> nul 2> nul
-call del plugins\AuthMe\authme.log /q 1> nul 2> nul
-call del plugins\bStats\temp.txt /q 1> nul 2> nul
-call del plugins\AntiAura\logs\*.* /q 1> nul 2> nul
+if %auto_del_files% equ true (
+ if %clear_logs% equ true call del logs\*.* /q 1> nul 2> nul
+ call del plugins\NoCheatPlus\*.log /q 1> nul 2> nul
+ call del plugins\NoCheatPlus\*.lck /q 1> nul 2> nul
+ call del plugins\NoCheatPlus\*.log.* /q 1> nul 2> nul
+ call del plugins\AuthMe\authme.log /q 1> nul 2> nul
+ call del plugins\bStats\temp.txt /q 1> nul 2> nul
+ call del plugins\AntiAura\logs\*.* /q 1> nul 2> nul
+)
 
-call echo %yeniden_baslatiliyor%
-call timeout %delay% 2> nul
+if %auto_restart% equ true (
+ call echo %yeniden_baslatiliyor%
+ call timeout %delay% 2> nul
 
-goto start
+ goto start
+) else (
+ pause
+)
